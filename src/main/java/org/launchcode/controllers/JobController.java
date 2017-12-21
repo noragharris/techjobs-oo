23d53a6865020c4b2c22e3364b30b6e49e069676
Nora Harris <noragharris@gmail.com>
2017-12-21 07:32:36 -0600

    completed tech jobs oo

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Employer;
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -23,7 +26,7 @@ public class JobController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, int id) {
 
-        // TODO #1 - get the Job with the given ID and pass it into the view
+        model.addAttribute("job",jobData.findById(id));
 
         return "job-detail";
     }
@@ -35,13 +38,22 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @Valid JobForm jobForm, Errors errors,
+                      @RequestParam String name, int employerId,
+                      int locationId, int positionTypeId, int coreCompetencyId) {
 
-        // TODO #6 - Validate the JobForm model, and if valid, create a
-        // new Job and add it to the jobData data store. Then
-        // redirect to the job detail view for the new Job.
+        if (errors.hasErrors()) {
+            model.addAttribute(new JobForm());
+            return "new-job";
+        }
 
-        return "";
+        Job newJob = new Job(name, jobData.getEmployers().findById(employerId),
+                jobData.getLocations().findById(locationId),
+                jobData.getPositionTypes().findById(positionTypeId),
+                jobData.getCoreCompetencies().findById(coreCompetencyId));
+        jobData.add(newJob);
+        model.addAttribute("job",jobData.findById(newJob.getId()));
+        return "job-detail";
 
     }
 }
